e771fb663b2b57845a62eb9dd72d2782e6ff3f0f
Justin Narvaez <159674525+JustinNarvaez@users.noreply.github.com>
2026-02-08 18:55:39 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/",
+  site: "https://JustinNarvaez.github.io",
 
   vite: {
     plugins: [tailwindcss()]
